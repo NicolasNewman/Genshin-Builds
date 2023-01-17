4c3bc03a8f7b587c4e150b696a45a1067db0b94d
NicolasNewman <nanewman01@gmail.com>
2023-01-17 09:30:20 -0600

    implemented root layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot></slot>
+
+<style>
+    :global(body) {
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+        background-image: url(/bg.jpg);
+        background-size: cover;   
+    }
+</style>
