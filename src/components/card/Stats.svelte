c74a8ba817a7a6f27d6f8b034b64b1ce19d4fa46
NicolasNewman <nanewman01@gmail.com>
2023-01-18 11:29:40 -0600

    added Stats component

---
+++ src/components/card/Stats.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { getAcensionLevel, seperateCamelCase } from '$lib/helper';
+	import type { IBuild } from '../../types/build';
+    export let build: IBuild;
+
+    const weapon = build.weapon;
+</script>
+
+<div class="relative h-full z-10">
+    <div class="info flex justify-between flex-col h-full text-lg">
+        <div class="flex items-center mt-4">
+            <img class="w-16 h-16" src="/weapons/{weapon?.weapon}.png" alt="Weapon Icon">
+            <div class="flex flex-col items-center">
+                <div class="text-center">{seperateCamelCase(weapon?.weapon ?? '')}</div>
+                <div class="flex">
+                    <div class="text-sm p-1 m-1 rounded bg-white/[.2]">Lvl. {weapon?.level}/<span class="text-xs">{getAcensionLevel(weapon?.ascension ?? 0)}</span></div>
+                    <div class="text-sm p-1 m-1 rounded bg-white/[.2]">R{weapon?.refinement}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
