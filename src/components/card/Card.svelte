2198c6d3820b1f6e5942333bef8330cd62b297ae
NicolasNewman <nanewman01@gmail.com>
2023-01-18 11:29:55 -0600

    updated Card to contain new sub-components

--- src/components/card/Card.svelte
+++ src/components/card/Card.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
     import Paper, { Content } from '@smui/paper';
 	import type { IBuild } from '../../types/build';
+	import Artifacts from './Artifacts.svelte';
 	import Character from './Character.svelte';
+	import Stats from './Stats.svelte';
     export let build: IBuild;
 </script>
 
-<div class="paper-container m-8 w-[600px] h-[300px]">
+<div class="paper-container m-8 w-[600px] h-[448px]">
     <div class="flex min-h-full paper smui-paper smui-paper--raised smui-paper--elevation-z1 smui-paper--rounded">
             <div class="w-1/3 min-h-full">
                 <Character build={build} />
             </div>
-            <div class="w-1/3 min-h-full"></div>
-            <div class="w-1/3 min-h-full"></div>
+            <div class="w-1/3 min-h-full">
+                <Stats build={build} />
+            </div>
+            <div class="w-1/3 min-h-full">
+                <Artifacts build={build} />
+            </div>
     </div>
 </div>
 
