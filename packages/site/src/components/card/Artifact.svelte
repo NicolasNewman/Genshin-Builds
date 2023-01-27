b15ee684e35a348dbb6e4bcbee1b21b6d1ed3245
NicolasNewman <nanewman01@gmail.com>
2023-01-27 11:48:31 -0600

    converted from listing stat names to icons

--- packages/site/src/components/card/Artifact.svelte
+++ packages/site/src/components/card/Artifact.svelte
@@ -13,7 +13,7 @@
 	<div class="relative z-10 my-2">
 		<div class="info flex justify-between flex-col h-full text-lg">
 			<div class="flex">
-				<div class="relative w-20 h-20 mr-2">
+				<div class="relative min-w-[5rem] h-20 mr-2">
 					<img
 						class="w-full h-full absolute -z-10"
 						src="{base}/artifacts/{artifact.set ?? ''}_{slot}.png"
@@ -21,40 +21,59 @@
 					/>
 					<div class="h-full flex flex-col justify-around items-end">
 						{#if artifact.type === 'sands' || artifact.type === 'goblet' || artifact.type === 'circlet'}
-							<div>{StatToName[artifact.mainstat]}</div>
+							<div
+								class="stat-icon lg"
+								style="mask-image: url({base}/icons/{artifact.mainstat}.svg)"
+							/>
 						{:else if artifact.type === 'flower'}
-							<div>HP</div>
+							<div class="stat-icon lg" style="mask-image: url({base}/icons/hp.svg)" />
 						{:else}
-							<div>ATK</div>
+							<div class="stat-icon lg" style="mask-image: url({base}/icons/atk.svg)" />
 						{/if}
+
 						<div class="flex">
 							<div class="text-sm p-1 m-1 rounded bg-white/[.2] w-fit">+{artifact.level}</div>
 						</div>
 					</div>
 				</div>
-				<div class="text-sm grid grid-cols-2 grid-rows-2 gap-x-4">
+				<div class="text-sm flex flex-col w-full">
 					{#if artifact.substatOne}
-						<div>
-							<div>{StatToName[artifact.substatOne]}</div>
-							<div>{artifact.substatOneRoll}</div>
+						<div class="flex justify-between">
+							<div
+								class="stat-icon"
+								style="mask-image: url({base}/icons/{artifact.substatOne}.svg)"
+							/>
+							<span>{artifact.substatOneRoll}{artifact.substatOne?.includes('_') ? '%' : ''}</span>
 						</div>
 					{/if}
 					{#if artifact.substatTwo}
-						<div>
-							<div>{StatToName[artifact.substatTwo]}</div>
-							<div>{artifact.substatTwoRoll}</div>
+						<div class="flex justify-between">
+							<div
+								class="stat-icon"
+								style="mask-image: url({base}/icons/{artifact.substatTwo}.svg)"
+							/>
+							<span>{artifact.substatTwoRoll}{artifact.substatTwo?.includes('_') ? '%' : ''}</span>
 						</div>
 					{/if}
 					{#if artifact.substatThree}
-						<div>
-							<div>{StatToName[artifact.substatThree]}</div>
-							<div>{artifact.substatThreeRoll}</div>
+						<div class="flex justify-between">
+							<div
+								class="stat-icon"
+								style="mask-image: url({base}/icons/{artifact.substatThree}.svg)"
+							/>
+							<span
+								>{artifact.substatThreeRoll}{artifact.substatThree?.includes('_') ? '%' : ''}</span
+							>
 						</div>
 					{/if}
 					{#if artifact.substatFour}
-						<div>
-							<div>{StatToName[artifact.substatFour]}</div>
-							<div>{artifact.substatFourRoll}</div>
+						<div class="flex justify-between">
+							<div
+								class="stat-icon"
+								style="mask-image: url({base}/icons/{artifact.substatFour}.svg)"
+							/>
+							<span>{artifact.substatFourRoll}{artifact.substatFour?.includes('_') ? '%' : ''}</span
+							>
 						</div>
 					{/if}
 				</div>
@@ -66,5 +85,19 @@
 <style>
 	.info {
 		text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.9);
+	}
+	.stat-icon {
+		width: 16px;
+		height: 16px;
+		background-color: white;
+		mask-size: contain;
+		mask-position: center;
+		mask-repeat: no-repeat;
+		display: inline-block;
+	}
+
+	.stat-icon.lg {
+		width: 24px;
+		height: 24px;
 	}
 </style>
