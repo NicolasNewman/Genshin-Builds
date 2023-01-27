b41df54846f28bf73a244c702f9d301700c64a43
NicolasNewman <nanewman01@gmail.com>
2023-01-27 11:46:30 -0600

    added function for calculating a characters stat values

--- packages/site/src/lib/build.ts
+++ packages/site/src/lib/build.ts
@@ -1,10 +1,145 @@
 // import type { IBuild } from "src/types/build";
 // import type { IGOOD, StatKey, SubstatKey } from "src/types/good";
-import type { IBuild } from '../types/build';
+import { characterInfo, weaponInfo, type StatBuff } from 'shared';
+import type { IArtifact, IBuild } from '../types/build';
 import type { IGOOD, StatKey, SubstatKey } from '../types/good';
+import { mainStatValues } from '../types/stats';
+import { ascensionLevelRange } from './db';
 import { roundSubStat } from './helper';
 
-type JoinedStat = StatKey | SubstatKey;
+export const StatBuffToJoinedStat: { [key in StatBuff]: JoinedStat } = {
+	ATK: 'atk_',
+	'Anemo DMG Bonus': 'anemo_dmg_',
+	'CRIT DMG': 'critDMG_',
+	'CRIT Rate': 'critRate_',
+	'Cryo DMG Bonus': 'cryo_dmg_',
+	DEF: 'def_',
+	'Dendro DMG Bonus': 'dendro_dmg_',
+	'Electro DMG Bonus': 'electro_dmg_',
+	'Elemental Mastery': 'eleMas',
+	'Energy Recharge': 'enerRech_',
+	'Geo DMG Bonus': 'geo_dmg_',
+	HP: 'hp_',
+	'Healing Bonus': 'heal_',
+	'Hydro DMG Bonus': 'hydro_dmg_',
+	'Physical DMG Bonus': 'physical_dmg_',
+	'Pyro DMG Bonus': 'pyro_dmg_'
+};
+export const JoinedStatToName: { [key in JoinedStat]: string } = {
+	atk_: 'ATK%',
+	atk: 'ATK',
+	anemo_dmg_: 'Anemo DMG Bonus',
+	critDMG_: 'CRIT DMG',
+	critRate_: 'CRIT Rate',
+	cryo_dmg_: 'Cryo DMG Bonus',
+	def_: 'DEF%',
+	def: 'DEF',
+	dendro_dmg_: 'Dendro DMG Bonus',
+	electro_dmg_: 'Electro DMG Bonus',
+	eleMas: 'Elemental Mastery',
+	enerRech_: 'Energy Recharge',
+	geo_dmg_: 'Geo DMG Bonus',
+	hp_: 'HP%',
+	hp: 'HP',
+	heal_: 'Healing Bonus',
+	hydro_dmg_: 'Hydro DMG Bonus',
+	physical_dmg_: 'Physical DMG Bonus',
+	pyro_dmg_: 'Pyro DMG Bonus'
+};
+
+type StatMap = { [key in JoinedStat]: number };
+export const computeBuildStats = (build: IBuild): StatMap => {
+	const map: StatMap = {
+		hp: 0,
+		hp_: 0,
+		atk: 0,
+		atk_: 0,
+		def: 0,
+		def_: 0,
+		critDMG_: 0,
+		critRate_: 0,
+		eleMas: 0,
+		heal_: 0,
+		enerRech_: 0,
+		physical_dmg_: 0,
+		anemo_dmg_: 0,
+		cryo_dmg_: 0,
+		dendro_dmg_: 0,
+		electro_dmg_: 0,
+		geo_dmg_: 0,
+		hydro_dmg_: 0,
+		pyro_dmg_: 0
+	};
+	const { artifacts, character, weapon } = build;
+	const { circlet, flower, goblet, plume, sands } = artifacts;
+
+	// Character parse
+	const { statBuff, stats } = characterInfo[character.character];
+	const characterAscensionStat = StatBuffToJoinedStat[statBuff];
+	const range = ascensionLevelRange(character.ascension);
+	const characterBuffs = stats[character.ascension][character.level === range[1] ? 1 : 0];
+
+	map[characterAscensionStat] += characterBuffs.SP;
+	map['atk'] += characterBuffs.ATK;
+	map['def'] += characterBuffs.DEF;
+	map['hp'] += characterBuffs.HP;
+	if (weapon) {
+		const { statBuff, stats } = weaponInfo[weapon.weapon];
+		if (statBuff !== null && stats !== null) {
+			const weaponAscensionStat = StatBuffToJoinedStat[statBuff];
+			const range = ascensionLevelRange(weapon.ascension);
+			const weaponBuffs = stats[weapon.ascension][weapon.level === range[1] ? 1 : 0];
+
+			map[weaponAscensionStat] += weaponBuffs.SP;
+			map['atk'] += weaponBuffs.ATK;
+		}
+	}
+
+	const computeArtifactSubStats = (artifact: IArtifact<'flower' | 'circlet'> | undefined) => {
+		if (artifact) {
+			if (artifact.substatOne) {
+				map[artifact.substatOne] += artifact.substatOneRoll;
+			}
+			if (artifact.substatTwo) {
+				map[artifact.substatTwo] += artifact.substatTwoRoll;
+			}
+			if (artifact.substatThree) {
+				map[artifact.substatThree] += artifact.substatThreeRoll;
+			}
+			if (artifact.substatFour) {
+				map[artifact.substatFour] += artifact.substatFourRoll;
+			}
+		}
+	};
+	computeArtifactSubStats(flower);
+	computeArtifactSubStats(plume);
+	computeArtifactSubStats(goblet);
+	computeArtifactSubStats(sands);
+	computeArtifactSubStats(circlet);
+
+	const computeArtifactMainStats = (artifact: IArtifact<'circlet'> | undefined) => {
+		if (artifact?.set) {
+			const { mainstat, level, rarity } = artifact;
+			console.log(rarity);
+			console.log(level);
+			map[mainstat] += mainStatValues[rarity - 1][mainstat][level];
+		}
+	};
+	computeArtifactMainStats(goblet);
+	computeArtifactMainStats(sands);
+	computeArtifactMainStats(circlet);
+
+	// character edge cases
+	if (character.character === 'SangonomiyaKokomi') {
+		map.critRate_ -= 100.0;
+		if (map.critRate_ < 0) {
+			map.critDMG_ = 0;
+		}
+	}
+	return map;
+};
+
+export type JoinedStat = StatKey | SubstatKey;
 export const StatToName: { [key in JoinedStat]: string } = {
 	hp: 'HP',
 	hp_: 'HP%',
