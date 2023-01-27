1d4c589d0614fe148ed2b4c4b126e0d14c460ef8
NicolasNewman <nanewman01@gmail.com>
2023-01-27 11:48:49 -0600

    adjusted display of artifacts

--- packages/site/src/components/card/Artifacts.svelte
+++ packages/site/src/components/card/Artifacts.svelte
@@ -1,29 +1,36 @@
 <script lang="ts">
 	import type { IBuild } from '../../types/build';
 	import Artifact from './Artifact.svelte';
-    export let build: IBuild;
+	export let build: IBuild;
 
-    const artifacts = build.artifacts;
-    const {circlet, flower, goblet, plume, sands} = artifacts;
+	const artifacts = build.artifacts;
+	const { circlet, flower, goblet, plume, sands } = artifacts;
 </script>
 
-{#if flower}
-    <Artifact artifact={flower} slot="flower"/>
-{/if}
-{#if plume}
-    <Artifact artifact={plume} slot="plume"/>
-{/if}
-{#if circlet}
-    <Artifact artifact={circlet} slot="circlet"/>
-{/if}
-{#if goblet}
-    <Artifact artifact={goblet} slot="goblet"/>
-{/if}
-{#if sands}
-    <Artifact artifact={sands} slot="sands"/>
-{/if}
+<div class="flex items-center">
+	<div class="w-[155px] mr-2">
+		{#if circlet}
+			<Artifact artifact={circlet} slot="circlet" />
+		{/if}
+		{#if goblet}
+			<Artifact artifact={goblet} slot="goblet" />
+		{/if}
+		{#if sands}
+			<Artifact artifact={sands} slot="sands" />
+		{/if}
+	</div>
+	<div class="w-[155px]">
+		{#if flower}
+			<Artifact artifact={flower} slot="flower" />
+		{/if}
+		{#if plume}
+			<Artifact artifact={plume} slot="plume" />
+		{/if}
+	</div>
+</div>
+
 <style>
-    .info {
-        text-shadow: 1px 1px 2px rgba(0,0,0,0.9);
-    }
+	.info {
+		text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.9);
+	}
 </style>
