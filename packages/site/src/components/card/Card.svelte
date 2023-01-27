75df2632e60ff61e142d86898b10fbc6b92ea193
NicolasNewman <nanewman01@gmail.com>
2023-01-27 11:49:37 -0600

    improved display of multiple cards

--- packages/site/src/components/card/Card.svelte
+++ packages/site/src/components/card/Card.svelte
@@ -1,32 +1,34 @@
 <script lang="ts">
-    import Paper, { Content } from '@smui/paper';
+	import Paper, { Content } from '@smui/paper';
 	import type { IBuild } from '../../types/build';
 	import Artifacts from './Artifacts.svelte';
 	import Character from './Character.svelte';
 	import Stats from './Stats.svelte';
-    export let build: IBuild;
+	export let build: IBuild;
 </script>
 
-<div class="paper-container m-8 w-[600px] h-[448px]">
-    <div class="flex min-h-full paper smui-paper smui-paper--raised smui-paper--elevation-z1 smui-paper--rounded">
-            <div class="w-1/3 min-h-full">
-                <Character build={build} />
-            </div>
-            <div class="w-1/3 min-h-full">
-                <Stats build={build} />
-            </div>
-            <div class="w-1/3 min-h-full">
-                <Artifacts build={build} />
-            </div>
-    </div>
+<div class="paper-container w-[740px] h-[272px]">
+	<div
+		class="flex min-h-full paper smui-paper smui-paper--raised smui-paper--elevation-z1 smui-paper--rounded"
+	>
+		<div class="w-[200px] min-h-full">
+			<Character {build} />
+		</div>
+		<div class="w-[200px] min-h-full">
+			<Stats {build} />
+		</div>
+		<div class="w-[340px] min-h-full">
+			<Artifacts {build} />
+		</div>
+	</div>
 </div>
 
 <style>
-    .paper-container {
-        font-family: 'Noto Serif', serif;
-    }
+	.paper-container {
+		font-family: 'Noto Serif', serif;
+	}
 
-    .paper {
-        padding: 0 !important;
-    }
+	.paper {
+		padding: 0 !important;
+	}
 </style>
--- packages/site/src/routes/[build]/+page.svelte
+++ packages/site/src/routes/[build]/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-    import LayoutGrid, { Cell } from '@smui/layout-grid';
+	import LayoutGrid, { Cell } from '@smui/layout-grid';
 	import { decode } from '$lib/encode';
 	import Card from '../../components/card/Card.svelte';
-    import type {PageData} from './$types'
+	import type { PageData } from './$types';
 
-    export let data: PageData;
+	export let data: PageData;
 
-
-    let builds = decode(data.build);
-    console.log(builds);
+	let builds = decode(data.build);
+	console.log(builds);
 </script>
 
-<div>
-    {#each builds as build}
-            <Card build={build}/>
-    {/each}
+<div class="grid grid-cols-2 justify-items-center gap-y-8 h-screen overflow-y-scroll py-4">
+	{#each builds as build}
+		<Card {build} />
+	{/each}
 </div>
