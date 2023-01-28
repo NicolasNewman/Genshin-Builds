ea926111a4c29ca5aeaedd9057a45acffe7cb89e
NicolasNewman <nanewman01@gmail.com>
2023-01-28 10:45:20 -0600

    cards are now responsive

--- packages/site/src/routes/[build]/+page.svelte
+++ packages/site/src/routes/[build]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import LayoutGrid, { Cell } from '@smui/layout-grid';
 	import { decode } from '$lib/encode';
 	import Card from '../../components/card/Card.svelte';
 	import type { PageData } from './$types';
@@ -10,7 +9,9 @@
 	console.log(builds);
 </script>
 
-<div class="grid grid-cols-2 justify-items-center gap-y-8 h-screen overflow-y-scroll py-4">
+<div
+	class="grid grid-cols-1 2xl:grid-cols-2 justify-items-center gap-y-8 h-screen overflow-scroll py-4"
+>
 	{#each builds as build}
 		<Card {build} />
 	{/each}
