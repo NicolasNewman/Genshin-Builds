42b4f1b678d9bb7d740c02f8f3d69b606d1133db
NicolasNewman <nanewman01@gmail.com>
2023-01-28 10:45:10 -0600

    replace app.css with app.scss, added elment colors

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { base } from '$app/paths';
-	import '../app.css';
+	import '../app.scss';
 </script>
 
 <svelte:head>
@@ -14,12 +14,23 @@
 </svelte:head>
 <slot />
 
-<style>
+<style lang="scss">
+	@use '../app.scss';
 	:global(body) {
 		margin: 0;
 		width: 100vw;
 		height: 100vh;
 		background-image: url(/bg.jpg);
 		background-size: cover;
+	}
+
+	:global(:root) {
+		--Cryo: linear-gradient(180deg, #{app.$cryo-dark} 50%, #{app.$cryo} 100%);
+		--Electro: linear-gradient(180deg, #{app.$electro-dark} 50%, #{app.$electro} 100%);
+		--Geo: linear-gradient(180deg, #{app.$geo-dark} 50%, #{app.$geo} 100%);
+		--Dendro: linear-gradient(180deg, #{app.$dendro-dark} 50%, #{app.$dendro} 100%);
+		--Pyro: linear-gradient(180deg, #{app.$pyro-dark} 50%, #{app.$pyro} 100%);
+		--Hydro: linear-gradient(180deg, #{app.$hydro-dark} 50%, #{app.$hydro} 100%);
+		--Anemo: linear-gradient(180deg, #{app.$anemo-dark} 50%, #{app.$anemo} 100%);
 	}
 </style>
