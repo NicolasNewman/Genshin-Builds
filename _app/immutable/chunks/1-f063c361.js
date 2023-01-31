ab7bfc418f899b0688dc9f41b646dbc46ee76dda
NicolasNewman <nanewman01@gmail.com>
2023-01-31 17:43:47 -0600

    Updates

--- _app/immutable/chunks/1-f063c361.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-2ec6e00b.js";export{t as component};
--- _app/immutable/chunks/2-ad4cc9e9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-396ca6d4.js";export{t as component};
