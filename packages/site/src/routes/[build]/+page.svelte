463ec4ff16e33ffbe6e1e9f23e741b564b5b542d
NicolasNewman <nanewman01@gmail.com>
2023-02-01 15:27:42 -0600

    improved styling if only 1 card was present

--- packages/site/src/routes/[build]/+page.svelte
+++ packages/site/src/routes/[build]/+page.svelte
@@ -5,14 +5,20 @@
 	export let data: PageData;
 
 	let builds = decode(data.build);
+
+	const singleCol = builds.length <= 1;
+	const dynamicGrid = singleCol ? 'grid-cols-1 w-fit mx-auto' : 'grid-cols-1 2xl:grid-cols-2';
+	const dynamicContainer = singleCol ? 'flex items-center' : '';
 	console.log(builds);
 </script>
 
-<div
-	id="card-container"
-	class="grid grid-cols-1 2xl:grid-cols-2 justify-items-start md:justify-items-center gap-y-8 py-4"
->
-	{#each builds as build}
-		<Card {build} />
-	{/each}
+<div class="min-h-[55.05%] {dynamicContainer}">
+	<div
+		id="card-container"
+		class="grid {dynamicGrid} justify-items-start md:justify-items-center gap-y-8 py-4"
+	>
+		{#each builds as build}
+			<Card {build} />
+		{/each}
+	</div>
 </div>
