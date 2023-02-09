7a68ed554298c78be4c5ad7605402b8a8a9b163b
NicolasNewman <nanewman01@gmail.com>
2023-02-09 14:13:05 -0600

    fixed errors with inventory encoding scheme

--- packages/site/src/lib/encode/invEncode.ts
+++ packages/site/src/lib/encode/invEncode.ts
@@ -16,24 +16,19 @@ import { b2to64, b64to2 } from './scheme';
  * @returns encoded strings of character and weapon data
  */
 export function encode(data: IGOOD) {
-	console.log(data.characters);
-	data.characters?.forEach((character) =>
-		console.log(`${character.key}: ${characterIDs[character.key]}`)
-	);
-	console.log(data.weapons);
 	const characters = data.characters
 		?.map((character) =>
 			b2to64(
-				`${binPad(characterIDs[character.key].toString(2), 7)}\
-                ${binPad(character.constellation.toString(2), 3)}`
+				`${binPad(characterIDs[character.key].toString(2), 9)}\
+${binPad(character.constellation.toString(2), 3)}`
 			)
 		)
 		.join('');
 	const weapons = data.weapons
 		?.map((weapon) =>
 			b2to64(
-				`${binPad(weaponIDs[weapon.key].toString(2), 8)}\
-                ${binPad(weapon.refinement.toString(2), 3)}`
+				`${binPad(weaponIDs[weapon.key].toString(2), 9)}\
+${binPad(weapon.refinement.toString(2), 3)}`
 			)
 		)
 		.join('');
@@ -49,27 +44,29 @@ export function encode(data: IGOOD) {
  * @returns decoded list of characters+constelations and weapons+refinement
  */
 export function decode(characters: string, weapons: string) {
-	const encodedCharacters = characters.match(/.{1,12}/g) || [];
-	const encodedWeapons = weapons.match(/.{1,12}/g) || [];
+	const encodedCharacters = characters.match(/.{1,2}/g) || [];
 	const ownedCharacters: { character: CharacterKey; constelation: number }[] =
 		encodedCharacters.map((character) => {
 			const bin = b64to2(character);
-			const cID = parseInt(bin.substring(0, 8), 2);
-			const cons = parseInt(bin.substring(8, 11), 2);
+			console.log(bin);
+			const cID = parseInt(bin.substring(0, 9), 2) - 1;
+			const cons = parseInt(bin.substring(9), 2);
 			return {
 				character: characterList[cID],
 				constelation: cons
 			};
 		});
+	const encodedWeapons = weapons.match(/.{1,2}/g) || [];
 	const ownedWeapons: { weapon: WeaponKey; refinement: number }[] = encodedWeapons.map((weapon) => {
 		const bin = b64to2(weapon);
-		const wID = parseInt(bin.substring(0, 9), 2);
-		const refinement = parseInt(bin.substring(9, 12), 2);
+		const wID = parseInt(bin.substring(0, 9), 2) - 1;
+		const refinement = parseInt(bin.substring(9), 2);
 		return {
 			weapon: weaponList[wID],
 			refinement: refinement
 		};
 	});
+
 	return {
 		characters: ownedCharacters,
 		weapons: ownedWeapons
